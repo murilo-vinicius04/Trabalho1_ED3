bd743738e85983b2d6e3a7d663a3a52832b81783
fuzao-jbl <108827743+fuzao-jbl@users.noreply.github.com>
2024-09-18 09:46:21 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "estruturas.c"
+
+
+int main()
+{
+    // primeiro recebemos as informacoes que precisamos
+    int n;
+    char nome[20];
+    scanf("%d %s", &n, nome);
+
+    // busca o arquivo
+    FILE *arquivo = fopen(nome, "r+");
+    if (arquivo == NULL)
+    {
+        printf("Falha no processamento do arquivo\n");
+        return 1;
+    }
+
+    // executa funcionalidade desejada
+    switch (n)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+    }
+
+    // fecha o arquivo
+    fclose(arquivo);
+
+    return 0;
+}
